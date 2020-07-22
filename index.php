9eb5c8c7daf69c78873a45f09bc00ed981ff9782
Dario Lopes <lopesdario6975@gmail.com>
2020-07-22 11:30:28 -0300

    comit inicial

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title>Formulário de inscrição</title>
+	<meta name="author" content="">
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=]">
+</head>
+
+<body>
+
+<p>FORMULARIO PARA INSCRIÇÃO DE COMPETIDORES</p>
+
+<form action="script.php" method="post">
+<!-- <form action="script.php" method="get"> -->
+<!-- Testando POST  e GET -->
+	<p>Seu nome: <input type="text" name="nome" /></p>
+	<p>Sua idade: <input type="text" name="idade" /></p>
+	<p><input type="submit" value="Enviar dados do competidor"/></p>
+	<!-- O input cria o botão onde os dados serão enviados. -->	
+</form>
+
+</body>
+
+</html>
